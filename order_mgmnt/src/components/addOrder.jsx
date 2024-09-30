9177f30af0cb99d2ef92033a785608caf9137524
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-09-30 02:39:42 -0400

    adding order sizes12

--- order_mgmnt/src/components/addOrder.jsx
+++ order_mgmnt/src/components/addOrder.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {  Modal, Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const AddOrder = () => {
   const [orderNumber, setOrderNumber] = useState('');
