e890c24f35dc2842b06a478a203231140b5476a0
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-10-21 07:26:01 -0400

    commit back

--- order_mgmnt/src/App.js
+++ order_mgmnt/src/App.js
@@ -4,20 +4,17 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import jsPDF from 'jspdf';
-import 'jspdf-autotable';
-
+// Import components
 import NavigationBar from "./components/navigationBar";
 import Sidenavigationbar from "./components/sidenavigationbar";
 import Footer from "./components/footer";
 import Entry from "./components/entry";
 import AddOrder from "./components/addOrder";
 import OrderList from "./components/orderList";
-
 import Riz from "./components/riz";
 import Karachi from "./components/karachi";
 import Mussa from "./components/mussa";
-import Embroidory from "./components/embroidory"
+import Embroidory from "./components/embroidory";
 import Dtg from "./components/dtg";
 import ScreenPrinting from "./components/screenPrinting";
 import DtgEmd from "./components/dtgEmd";
@@ -31,7 +28,7 @@ import SpEmd from "./components/spEmb";
 import CompletedInvoice from "./components/completedInvoice";
 import Pullsheet from "./components/pullsheet";
 
-
+// Import ErrorBoundary component
 
 
 function App() {
@@ -40,30 +37,38 @@ function App() {
 
   return (
     <div className="App">
+      {/* Navigation Bar */}
       <NavigationBar />
+
+      {/* Sidebar conditionally shown on all routes except root */}
       {showSidebar && <Sidenavigationbar />}
-      <Routes>
-        <Route path='/' element={<Entry />} />
-        <Route path='/Home' element={<Home />} />
-        <Route path='/addOrder' element={<AddOrder />} />
-        <Route path='/orderList' element={<OrderList />} />
-        <Route path='/riz' element={<Riz />} />
-        <Route path='/karachi' element={<Karachi />} />
-        
-        <Route path = '/mussa' element = {<Mussa/>}/>
-        <Route path= '/bob' element = {<Bob/>}/>
-        <Route path = '/embroidory' element = {<Embroidory/>}/>
-        <Route path= '/dtg' element = {< Dtg/>}/>
-        <Route path= '/screenPrinting' element = {< ScreenPrinting/>}/>
-        <Route path= '/dtgEmd' element = {<DtgEmd/>}/>
-        <Route path= '/spEmd' element = {<SpEmd/>}/>
-        <Route path = '/instore' element = {<Instore/>}/>
-        <Route path = '/packing' element = {<Packing/>}/>
-        <Route path = '/completed' element = {<Completed/>}/>
-        <Route path = '/invoice' element = {<Invoice/>}/>
-        <Route path = '/completedInvoice' element = {<CompletedInvoice/>}/>
-        <Route path = '/pullsheet' element = {<Pullsheet/>}/>
-      </Routes>
+
+      {/* Wrap Routes inside ErrorBoundary */}
+   
+        <Routes>
+          <Route path='/' element={<Entry />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/addOrder' element={<AddOrder />} />
+          <Route path='/orderList' element={<OrderList />} />
+          <Route path='/riz' element={<Riz />} />
+          <Route path='/karachi' element={<Karachi />} />
+          <Route path='/mussa' element={<Mussa />} />
+          <Route path='/bob' element={<Bob />} />
+          <Route path='/embroidory' element={<Embroidory />} />
+          <Route path='/dtg' element={<Dtg />} />
+          <Route path='/screenPrinting' element={<ScreenPrinting />} />
+          <Route path='/dtgEmd' element={<DtgEmd />} />
+          <Route path='/spEmd' element={<SpEmd />} />
+          <Route path='/instore' element={<Instore />} />
+          <Route path='/packing' element={<Packing />} />
+          <Route path='/completed' element={<Completed />} />
+          <Route path='/invoice' element={<Invoice />} />
+          <Route path='/completedInvoice' element={<CompletedInvoice />} />
+          <Route path='/pullsheet' element={<Pullsheet />} />
+        </Routes>
+      
+
+      {/* Footer */}
       <Footer />
     </div>
   );
