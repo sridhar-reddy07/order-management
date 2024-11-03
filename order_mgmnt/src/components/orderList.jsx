46064aaf9bf4e427dbebc226d92f03bf201a876f
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-11-03 01:44:42 -0500

    files theme changed

--- order_mgmnt/src/components/orderList.jsx
+++ order_mgmnt/src/components/orderList.jsx
@@ -614,7 +614,7 @@ const handleDeleteFile = async (file ,index, id) => {
 
                 </tr>
                 <tr>
-                  <td colSpan="6">
+                  <td colSpan="12">
                     <Collapse in={openOrder === order.orderNumber}>
                       <div>
                         <p><strong>Shipping Address:</strong> {order.shippingAddress}
