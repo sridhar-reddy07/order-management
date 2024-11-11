c651267cbda069374cc88e4bf54f4319720d2558
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-11-11 02:09:12 -0500

    user access deleted

--- order_mgmnt/src/components/orderList.jsx
+++ order_mgmnt/src/components/orderList.jsx
@@ -522,7 +522,7 @@ function cleanFileName(url) {
               <React.Fragment key={index}>
                 <tr  
                 style={{
-                  backgroundColor: order.jobType === 'DTG' ? 'yellow' : 'transparent'
+                  backgroundColor: order.jobType.trim() === 'DTG+EMB' ? 'yellow' : 'transparent'
                 }} >
                   <td className="order-cell"  onClick={() => handleOrderClick(order.orderNumber,order.id)}>
                     
