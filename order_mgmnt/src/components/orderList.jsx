7127380448f779ddd38062a671a75d6882f5609d
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-11-11 00:07:06 -0500

    changed garment detail

--- order_mgmnt/src/components/orderList.jsx
+++ order_mgmnt/src/components/orderList.jsx
@@ -518,8 +518,8 @@ function cleanFileName(url) {
             console.log('Current Order Status:', order.orderStatus); // Debug log
             return (
               <React.Fragment key={index}>
-                <tr  >
-                  <td className="order-cell" onClick={() => handleOrderClick(order.orderNumber,order.id)}>
+                <tr onMouseDown={() => handleOrderClick(order.orderNumber,order.id)}  >
+                  <td className="order-cell" >
                     
                     {order.orderNumber}
                   </td>
