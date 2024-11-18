f1e1a1455616d0d7be310786702a19de402975c5
sridhar-reddy07 <sridharreddynimmakayala@gmail.com>
2024-11-18 01:36:15 -0500

    made completed editable

--- Backend/server.js
+++ Backend/server.js
@@ -1246,7 +1246,7 @@ app.get('/karachiList', (req, res) => {
 
   app.get('/completedList', (req, res) => {
     const search = req.query.search || '';  // Get the search query from the request
-    const fromDate = req.query.fromDate || '';  // Default start date if not provided
+    const fromDate = req.query.fromDate || '1970-01-01';  // Default start date if not provided
     const toDate = req.query.toDate || new Date().toISOString().slice(0, 10);  // Default to today if not provided
     const adjustedToDate = `${toDate} 23:59:59`;
     const sql = `
